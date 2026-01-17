de84670217dbab82640e875a3dc05016f92a18d4
Michael Papismedov <dvnk.a2@gmail.com>
2026-01-17 06:07:21 +0200

    0.0.6

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import './index.css'
+import { ConsentProvider } from './contexts/ConsentContext'
 import { AuthProvider } from './contexts/AuthContext'
 import { CartProvider } from './contexts/CartContext'
 import { ToastProvider } from './contexts/ToastContext'
@@ -12,16 +13,18 @@ import { ContactModal } from './components/ContactModal'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ToastProvider>
-      <AuthProvider>
-        <CartProvider>
-          <ContactModalProvider>
-            <SplashScreen />
-            <RouterProvider router={router} />
-            <ContactModal />
-          </ContactModalProvider>
-        </CartProvider>
-      </AuthProvider>
-    </ToastProvider>
+    <ConsentProvider>
+      <ToastProvider>
+        <AuthProvider>
+          <CartProvider>
+            <ContactModalProvider>
+              <SplashScreen />
+              <RouterProvider router={router} />
+              <ContactModal />
+            </ContactModalProvider>
+          </CartProvider>
+        </AuthProvider>
+      </ToastProvider>
+    </ConsentProvider>
   </React.StrictMode>,
 )
